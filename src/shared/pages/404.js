39a71f848a01d07e8fffca77ba8ba99fc3de146a
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-10-16 01:46:37 -0400

    updare formatting

--- src/shared/pages/404.js
+++ src/shared/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PageNotFound = () => {
-    return <h1>Error: Page Not Found</h1>;
-}
+  return <h1>Error: Page Not Found</h1>;
+};
 
-export default PageNotFound
+export default PageNotFound;

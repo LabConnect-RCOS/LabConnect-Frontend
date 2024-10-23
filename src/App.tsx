9b9da274d9ff45a00438dac0495579fcf3665d66
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-10-23 00:55:40 -0400

    clean up auth items

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./shared/pages/Home.tsx";
 import PageNotFound from "./shared/pages/404.tsx";
-import MainNavigation from "./shared/components/Navigation/MainNavigation.js";
+import MainNavigation from "./shared/components/Navigation/MainNavigation.tsx";
 import Jobs from "./opportunities/pages/Jobs.js";
 import Departments from "./staff/pages/Departments.tsx";
 import Profile from "./staff/pages/Profile.tsx";
@@ -11,58 +11,26 @@ import Department from "./staff/pages/Department.tsx";
 import CreatePost from "./staff/pages/CreatePost.js";
 import IndividualPost from "./opportunities/pages/IndividualPost.js";
 import ProfilePage from "./shared/pages/Profile.js";
+import LoginRedirection from "./auth/Login.tsx";
+import LogoutRedirection from "./auth/Logout.tsx";
 import { GlobalContextProvider } from "./context/global/GlobalContextProvider.js";
 import StickyFooter from "./shared/components/Navigation/StickyFooter.js";
 
 function App() {
+
   const handleCallback = async () => {
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get("token");
     if (token) {
       localStorage.setItem("jwt", token);
+      localStorage.setItem("jwt-time", new Date().getTime().toString());
+      window.location.href = "/";
     }
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     handleCallback();
   }, []);
-
-  const LoginRedirection = () => {
-    window.location.href = `${process.env.REACT_APP_BACKEND_SERVER}/login`;
-    return null; // No need to render anything, as the redirection happens immediately
-  };
-
-  const LogoutRedirection = () => {
-    useEffect(() => {
-      const logout = async () => {
-        try {
-          const response = await fetch(
-            `${process.env.REACT_APP_BACKEND_SERVER}/logout`,
-            {
-              method: "GET",
-              credentials: "include", // to send cookies or session data
-            }
-          );
-          if (response.ok) {
-            // Clear local storage or tokens
-            localStorage.removeItem("jwt");
-            // Redirect to the homepage or login page
-            window.location.href = "/";
-          } else {
-            console.error("Failed to logout");
-          }
-        } catch (error) {
-          console.error("Error logging out:", error);
-        }
-      };
-
-      logout();
-      window.location.href = "/";
-    }, []); // Run only on component mount
-
-    // While logging out, you could return a loading message or just null
-    return null; // Since this component doesn't need to render anything
-  };
 
   return (
     <GlobalContextProvider>

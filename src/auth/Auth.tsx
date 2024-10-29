ff1f9cae54cad8b63992fe0acb99dc1e3880fb42
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-10-29 16:47:01 -0400

    Remove timing for auth

--- src/auth/Auth.tsx
+++ src/auth/Auth.tsx
@@ -1,17 +1,8 @@
 const IsAuthenticated = (): [string, boolean] => {
     const jwt = localStorage.getItem('jwt');
-    const jwtTime = localStorage.getItem('jwt-time');
 
-    if (!jwt || !jwtTime) {
+    if (!jwt) {
         return ["", false];
-    }
-
-    const currentTime = new Date().getTime();
-    const jwtTimeNumber = parseInt(jwtTime, 10);
-
-    // Check if the JWT time is within 1 hour (3600000 milliseconds)
-    if (currentTime - jwtTimeNumber > 3600000) {
-        return [jwt, false];
     }
 
     return [jwt, true];

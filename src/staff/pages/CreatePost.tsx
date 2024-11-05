4f26c740db05b627016847a2b1d924f623dfc95b
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-11-05 16:28:49 -0500

    Convert page to tsx

--- src/staff/pages/CreatePost.tsx
+++ src/staff/pages/CreatePost.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import CreationForms from "../components/CreationForms";
 import './CreatePost.css';
 
-const CreatePost = ({edit}) => {
+const CreatePost = ({ edit }) => {
   return (
     <div className="createpost">
       <div>
-        <h1>{edit===true ? "Edit Research Opportunity" : "Create Research Opportunity"}</h1>
+        <h1>{edit === true ? "Edit Research Opportunity" : "Create Research Opportunity"}</h1>
         <CreationForms />
       </div>
     </div>
   );
+};
+CreatePost.propTypes = {
+  edit: PropTypes.bool.isRequired,
 };
 
 export default CreatePost;

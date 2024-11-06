7495d834d15c76ca1db116c6f62b0d3f943d0b44
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-11-05 23:47:07 -0500

    Update page with SEO and arguments

--- src/staff/pages/CreatePost.tsx
+++ src/staff/pages/CreatePost.tsx
@@ -1,20 +1,28 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import CreationForms from "../components/CreationForms";
+import CreationForms from "../components/CreationForms.tsx";
+import SEO from "../../shared/components/SEO.tsx";
 import './CreatePost.css';
 
-const CreatePost = ({ edit }) => {
+const CreatePost = ({ edit, authenticated }) => {
+  if (!authenticated[1]) {
+    window.location.href = "/login";
+  }
+
   return (
-    <div className="createpost">
-      <div>
-        <h1>{edit === true ? "Edit Research Opportunity" : "Create Research Opportunity"}</h1>
-        <CreationForms />
-      </div>
+    <div className="w-9/12 mx-auto">
+      <SEO title={edit === true ? "Edit Research Opportunity" : "Create Research Opportunity"} description={edit === true ? "Edit Research Opportunity Page" : "Create Research Opportunity Page"} />
+      <h1 className="text-center my-8 text-3xl font-bold">{edit === true ? "Edit Research Opportunity" : "Create Research Opportunity"}</h1>
+      <CreationForms edit={edit} token={authenticated[0]} />
     </div>
   );
 };
 CreatePost.propTypes = {
   edit: PropTypes.bool.isRequired,
+  authenticated: PropTypes.arrayOf(PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.bool
+  ])).isRequired
 };
 
 export default CreatePost;

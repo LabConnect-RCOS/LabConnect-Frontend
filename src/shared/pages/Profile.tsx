737aff83dae5b8c722b12a50c2bfc94cf426bc9d
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-11-22 18:41:52 -0500

    Complete profile page

--- src/shared/pages/Profile.tsx
+++ src/shared/pages/Profile.tsx
@@ -1,37 +1,54 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import ProfileComponents from "../components/Profile/ProfileComponents.tsx";
+import { useAuth } from "../../context/AuthContext.tsx";
 // import EditProfile from "./EditProfile";
 
-const ProfilePage = (authenticated) => {
+export default function ProfilePage() {
+  const { auth } = useAuth();
 
-  if (authenticated[0] === false) {
+  if (!auth.isAuthenticated) {
     window.location.href = "/login";
   }
 
   // const [editMode, setEditMode] = useState(false);
-  const [profile, setProfile] = useState<null | boolean>(null);
+  interface Profile {
+    id: string;
+    name: string;
+    image: string;
+    department: string;
+    description: string;
+    website?: string;
+  }
+
+  const [profile, setProfile] = useState<null | Profile | boolean>(null);
 
   // const changeEditMode = () => {
   //   setEditMode(!editMode);
   // };
 
-  const fetchProfile = async () => {
-    const response = await fetch(
-      `${process.env.REACT_APP_BACKEND_SERVER}/profile`
-    );
-
-    if (response.ok) {
-      const data = await response.json();
-      setProfile(data);
-    } else {
-      setProfile(false);
-    }
-  };
-
   useEffect(() => {
+    const fetchProfile = async () => {
+      const response = await fetch(
+        `${process.env.REACT_APP_BACKEND_SERVER}/profile`, {
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
+        },
+      }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        // if (data.lab_manager) {
+        //   window.location.href = "/staff/" + data.id;
+        // }
+        setProfile(data);
+      } else {
+        setProfile(false);
+      }
+    };
     fetchProfile();
-  }, []);
+  }, [auth.token]);
 
   // const editButton = (
   //   <button className="btn btn-primary my-3" onClick={changeEditMode}>
@@ -40,12 +57,10 @@ const ProfilePage = (authenticated) => {
   // );
 
   return (
-    <>
+    <section className="center container-xl">
       {profile === null && "Loading..."}
-      {profile && typeof profile === "object" && <ProfileComponents profile={profile} />}
+      {profile && typeof profile === "object" && <ProfileComponents profile={profile} id={profile.id} staff={false} />}
       {profile === false && "Profile not found"}
-    </>
+    </section>
   );
 };
-
-export default ProfilePage;

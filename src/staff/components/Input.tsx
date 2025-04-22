7f24804096e8d86e8c760ffc89ba8e3c7934ea34
Devan Patel <devankushpatel@gmail.com>
2025-04-22 19:34:58 -0400

    Updated the input.js file to be a typescript file
    Deleted the input.js file, added a Input.tsx file, and converted the code

--- src/staff/components/Input.tsx
+++ src/staff/components/Input.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Input = ({
+interface InputProps {
+  type?: "text" | "number" | "select" | "textarea";
+  errorMessage?: string;
+  errors?: Record<string, any>;
+  name: string;
+  formHook: any; // Can be made more specific with `UseFormRegisterReturn` if desired
+  label?: string;
+  options?: string[];
+  placeHolder?: string;
+  min?: number;
+  step?: number;
+}
 
+const Input: React.FC<InputProps> = ({
   type,
   errorMessage,
   errors,
@@ -11,28 +23,26 @@ const Input = ({
   options,
   placeHolder,
   min,
-
+  step,
 }) => {
-
   const inputElement = (
-    
     <input
       {...formHook}
       type={type || "text"}
       list={options && name}
       placeholder={placeHolder || "Type Here"}
-      className="input input-bordered w-full"
+      className="input input-bordered w-full appearance-none"
       min={type === "number" ? min : undefined}
+      step={type === "number" ? step : undefined}
     />
-
   );
 
   const textAreaElement = (
     <textarea
       {...formHook}
       placeholder={placeHolder || "Type Here"}
-      cols="50"
-      rows="7"
+      cols={50}
+      rows={7}
       className="border-2 rounded p-2 m-0"
     ></textarea>
   );
@@ -40,10 +50,10 @@ const Input = ({
   const selectElement = (
     <select
       {...formHook}
-      defaultValue="Select a department"
+      defaultValue=""
       className="select select-bordered w-full"
     >
-      <option value="" selected disabled hidden>
+      <option value="" disabled hidden>
         Select a department
       </option>
       {options ? (

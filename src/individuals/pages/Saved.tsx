defd1a6262094a9610531b096df7d0cce9ea237c
doan-neyugn <doantnguyen04@gmail.com>
2025-10-23 15:38:43 -0400

    Merge

--- src/individuals/pages/Saved.tsx
+++ src/individuals/pages/Saved.tsx
@@ -18,15 +18,7 @@ export default function SavedPage() {
     const fetchSaved = async () => {
         try {
             const response = await fetch(
-<<<<<<< HEAD
-<<<<<<< HEAD
                 `${import.meta.env.VITE_BACKEND_SERVER}/savedOpportunities`, {
-=======
-                `${process.env.REACT_APP_BACKEND_SERVER}/savedOpportunities`, {
->>>>>>> 608bbc4 (Create page to show saved opportunities (#165))
-=======
-                `${import.meta.env.VITE_BACKEND_SERVER}/savedOpportunities`, {
->>>>>>> 40a1bab (Fix ENV vars (#166))
                 credentials: "include",
             }
             );
@@ -106,15 +98,7 @@ export default function SavedPage() {
                                             }
 
                                             const response = await fetch(
-<<<<<<< HEAD
-<<<<<<< HEAD
                                                 `${import.meta.env.VITE_BACKEND_SERVER}/unsaveOpportunity/${opportunity.id}`, {
-=======
-                                                `${process.env.REACT_APP_BACKEND_SERVER}/unsaveOpportunity/${opportunity.id}`, {
->>>>>>> 608bbc4 (Create page to show saved opportunities (#165))
-=======
-                                                `${import.meta.env.VITE_BACKEND_SERVER}/unsaveOpportunity/${opportunity.id}`, {
->>>>>>> 40a1bab (Fix ENV vars (#166))
                                                 method: "DELETE",
                                                 credentials: "include",
                                                 headers,
